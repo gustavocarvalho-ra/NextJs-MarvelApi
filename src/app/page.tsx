7dab19945309c6e2e357878eeb500d2b36b03794
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-31 18:39:08 -0300

    test of component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <p>
-        test
+        asdd
       </p>
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,11 @@
+"use client"
+
 import { A } from "./styles"
 
 export default function Header() {
   return (
     <A>
-
+      <h1>ksks</h1>
     </A>
   )
 }
