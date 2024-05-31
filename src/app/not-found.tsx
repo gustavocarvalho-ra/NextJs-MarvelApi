2804877ef1b3f61cb21a356c57d9e1e94ee2d3e1
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-30 23:54:04 -0300

    test image

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,10 @@ import Image from "next/image"
 export default function Notfound() {
   return (
     <>
-      <Image></Image>
-      <h2>404</h2>
+      <div style={{height: '100vh', width: '100vw'}}>
+        <Image src={"/not.png"} alt="Image 404" width={650} height={650} />
+        <h2>404</h2>
+      </div>
     </>
   )
 }
