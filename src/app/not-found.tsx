e1e0174cd23b157a871b6e0a52742539ea901813
GustavoCR021 <gustavoc1922@gmail.com>
2024-05-30 23:55:43 -0300

    feat: add Link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Notfound() {
   return (
     <>
       <div style={{height: '100vh', width: '100vw'}}>
         <Image src={"/not.png"} alt="Image 404" width={650} height={650} />
-        <h2>404</h2>
+        <Link href={'/'}>Aperte aqui para voltar</Link>
       </div>
     </>
   )
