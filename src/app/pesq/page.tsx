4dd07dbfa20ef1072f29e26a0b63709a1c18f549
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-02 19:28:52 -0300

    test response feath

--- src/app/pesq/page.tsx
+++ src/app/pesq/page.tsx
@@ -8,6 +8,17 @@ import { Bg } from "./styles";
 
 
 export default function Aaa() {
+  const timeStamp = '1717367046';
+  const Api = '1819332dc7d58586ce9a9bc528e0b7b2';
+  const hash = 'abf850a1524c59c9dadabcd98bf63de1';
+
+  fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${timeStamp}&apiKey=${Api}&hash=${hash}&limit=6`)
+  .then((response) => {
+    return response.json();
+  }).then((data) => {
+    console.log(data);
+  })
+
 
   return (
     <Bg>
