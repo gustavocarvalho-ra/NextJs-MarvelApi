9263b9185a7b47eab3ac3c9956b9f1151cd03d27
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-03 22:54:05 -0300

    at

--- src/app/pesq/page.tsx
+++ src/app/pesq/page.tsx
@@ -8,9 +8,9 @@ import { Bg } from "./styles";
 
 
 export default function Aaa() {
-  const timeStamp = '1717367046';
+  const timeStamp = '1717465875';
   const Api = '1819332dc7d58586ce9a9bc528e0b7b2';
-  const hash = 'abf850a1524c59c9dadabcd98bf63de1';
+  const hash = '0c2e2c494bf371cd4661eb29d1ad3931';
 
   fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${timeStamp}&apiKey=${Api}&hash=${hash}&limit=6`)
   .then((response) => {
