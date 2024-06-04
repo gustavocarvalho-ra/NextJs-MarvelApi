d63a03efbe29bdd2766f5a2ae423ab81c52c8573
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-04 01:30:25 -0300

    at

--- src/app/pesq/page.tsx
+++ src/app/pesq/page.tsx
@@ -5,15 +5,14 @@ import md5 from 'md5';
 
 import { Bg } from "./styles";
 
-
 export default function Aaa() {
   const time = Number(new Date());
-  const Api = '1819332dc7d58586ce9a9bc528e0b7b2';
-  // const hash = '0c2e2c494bf371cd4661eb29d1ad3931';
-  const hash = md5(time +  + Api);
+  const keyPu = '1819332dc7d58586ce9a9bc528e0b7b2';
+  const keyPr = 'fbdcd44109e6c3664d1217c42bc25417a9421498'
+  const hash = md5(time + keyPr + keyPu);
 
-
-  fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${time}&apikey=${Api}&hash=${hash}`
+  
+  const data = fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${time}&apikey=${keyPu}&hash=${hash}`
   ).then((response) => {
     return response.json();
   }).then((jsonParsed) => {
@@ -25,7 +24,12 @@ export default function Aaa() {
 
   return (
     <Bg>
-      
+      {data
+      .map(key => (
+        <div>
+          <p>`${key.id}</p>
+        </div>
+      ))}
       <p>
         asdd
       </p>
