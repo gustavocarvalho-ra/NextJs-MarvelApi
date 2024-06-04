a6470aaf94554e661cd6ed1c58b33afbd53215a5
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-04 00:24:05 -0300

    build: add "md5" dependency

--- src/app/pesq/page.tsx
+++ src/app/pesq/page.tsx
@@ -2,17 +2,17 @@
 
 import Image from "next/image";
 
-import LayoutOff from "./layout";
-import Header from "@/components/Header";
+
 import { Bg } from "./styles";
 
 
 export default function Aaa() {
-  const timeStamp = '1717465875';
+  const time = Number(new Date());
   const Api = '1819332dc7d58586ce9a9bc528e0b7b2';
   const hash = '0c2e2c494bf371cd4661eb29d1ad3931';
 
-  fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${timeStamp}&apikey=${Api}&hash=${hash}`
+
+  fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${time}&apikey=${Api}&hash=${hash}`
   ).then((response) => {
     return response.json();
   }).then((jsonParsed) => {
@@ -20,8 +20,11 @@ export default function Aaa() {
   })
 
 
+
+
   return (
     <Bg>
+      
       <p>
         asdd
       </p>
