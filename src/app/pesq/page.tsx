0d88049df963b68af5254ece1be22c5351754d6c
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-05 20:37:43 -0300

    test .map

--- src/app/pesq/page.tsx
+++ src/app/pesq/page.tsx
@@ -11,29 +11,31 @@ export default function Aaa() {
   const keyPr = 'fbdcd44109e6c3664d1217c42bc25417a9421498'
   const hash = md5(time + keyPr + keyPu);
 
-  async function htt() {
-    const data = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${time}&apikey=${keyPu}&hash=${hash}`
-    ).then((response) => {
-      return response.json();
-    }).then((jsonParsed) => {
-      console.log(jsonParsed);
-    })
-    
-  }
-  // const data = fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${time}&apikey=${keyPu}&hash=${hash}`
-  // ).then((response) => {
-  //   return response.json();
-  // }).then((jsonParsed) => {
-  //   console.log(jsonParsed);
-  // })
+  // async function htt(jsonParsed: string) {
+  //   const data = await fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${time}&apikey=${keyPu}&hash=${hash}`
+  //   ).then((response) => {
+  //     return response.json();
+  //   }).then((jsonParsed) => {
+  //     console.log(jsonParsed);
+  //   })
+  //   // console.log(data)
+  // }
+
+  
+  const data = fetch(`https://gateway.marvel.com:443/v1/public/characters?limit=6&ts=${time}&apikey=${keyPu}&hash=${hash}`
+  ).then((response) => {
+    return response.json();
+  }).then((jsonParsed:string) => {
+    console.log(jsonParsed);
+  })
 
 
   return (
     <Bg>
-      {
-      .map(key => (
+      {data
+      .map(item => (
         <div>
-          <p>`${key.id}`</p>
+          <p>`${item.id}`</p>
         </div>
       ))}
       <p>
