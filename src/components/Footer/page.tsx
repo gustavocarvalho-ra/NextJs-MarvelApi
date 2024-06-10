84ff6b36ca96e7b8b5d2fc81e4786a8650553859
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-10 20:24:08 -0300

    prod

---
+++ src/components/Footer/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import Link from "next/link";
+
+import { Foo } from "./styles"; 
+
+
+export default function Home() {
+
+  return (
+    <>
+      <div className="container">
+        <h3>
+          desenvoldido por: <Link href={"https://github.com/gustavocarvalho-ra"}>Gustavo</Link>
+        </h3>
+      </div>
+    </>
+  )
+}
