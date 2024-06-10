4b79c3ba89c012c6f3ad4f1167f375baa3b25974
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-10 20:39:57 -0300

    style: font-size

--- src/components/Foot/styles.ts
+++ src/components/Foot/styles.ts
@@ -9,10 +9,12 @@ export const Foo = styled.div `
     width: 100%;
     height: 100%;
     display: flex;
+    align-items: center;
     justify-content: center;
     background-color: #1F0409    ;
 
     h3 {
+      font-size: 14px;
       color: white;
     }
   }
