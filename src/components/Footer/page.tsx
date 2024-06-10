1433e05ad7df0791e9d0ab8ac04146ec8a6f841d
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-10 20:26:41 -0300

    .

--- src/components/Footer/page.tsx
+++ src/components/Footer/page.tsx
@@ -8,12 +8,12 @@ import { Foo } from "./styles";
 export default function Home() {
 
   return (
-    <>
+    <Foo>
       <div className="container">
         <h3>
           desenvoldido por: <Link href={"https://github.com/gustavocarvalho-ra"}>Gustavo</Link>
         </h3>
       </div>
-    </>
+    </Foo>
   )
 }
