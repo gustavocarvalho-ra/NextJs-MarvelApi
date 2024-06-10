cb39560d7568e85f8f70d353855d2d36d2c0a894
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-10 20:25:38 -0300

    style: width

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -2,5 +2,15 @@ import styled from "styled-components";
 
 
 export const Foo = styled.div `
+  width: 100vw;
+  height: 8vh;
 
+  .container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+
+    
+  }
 `
