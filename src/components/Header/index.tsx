6e2dd4e53b3bc02b28ff733c7328d57fc33c8958
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-19 21:41:18 -0300

    rename file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,6 +14,7 @@ export default function Header() {
         </Link>
       </A>
       <Nav>
+        <Link href={'/comic'}>Caracters</Link>
         <Link href={'/comic'}>Comics</Link>
       </Nav>
     </>
