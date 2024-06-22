1d4bafa282434c9d1af2ebaec5c8464724a92252
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-22 16:00:41 -0300

    feat: add link in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,13 @@ export default function Home() {
                 <IoSearch style={{marginTop: 6}}/>
               </button>
             </Link>
+
+            <Link className="hel" href={'/series'}>
+              <button>
+                Series <br/>
+                <IoSearch style={{marginTop: 6}}/>
+              </button>
+            </Link>
           </section>
         </div>
         <Image className="bg" src={"/nest.png"} alt="test" fill={true} />
