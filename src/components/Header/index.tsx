54596239cbea6b8983189b357755d4c71cb66406
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-23 21:40:17 -0300

    feat: add new link for page events

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -16,6 +16,7 @@ export default function Header() {
         <Link className="a" href={'/characters'}>characters</Link>
         <Link className="a" href={'/comic'}>comics</Link>
         <Link className="a" href={'/series'}>series</Link>
+        <Link className="a" href={'/even'}>events</Link>
       </Nav>
     </>
   )
