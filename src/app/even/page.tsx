a9515433fa17801c01fc0e325ceff5fd797888c5
GustavoCR021 <gustavoc1922@gmail.com>
2024-06-23 21:49:09 -0300

    test page

--- src/app/even/page.tsx
+++ src/app/even/page.tsx
@@ -21,7 +21,7 @@ export default function Prin() {
   const [data, setData] = useState<CaractersData[]>([]);
 
   async function logData() {
-    const test = search !== "" ? `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${search}&limit=100&ts=${time}&apikey=${keyPu}&hash=${hash}` : `https://gateway.marvel.com:443/v1/public/characters?limit=100&ts=${time}&apikey=${keyPu}&hash=${hash}`
+    const test = search !== "" ? `https://gateway.marvel.com:443/v1/public/events?orderBy=modified&nameStartsWith=${search}&limit=100&ts=${time}&apikey=${keyPu}&hash=${hash}` : `https://gateway.marvel.com:443/v1/public/events?orderBy=modified&limit=100&ts=${time}&apikey=${keyPu}&hash=${hash}`
 
     const response = await fetch (
       test
