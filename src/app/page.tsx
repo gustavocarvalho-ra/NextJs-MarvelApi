83298dbe7a8929da87e14f099d75e6a2b0dd1511
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-03 22:53:13 -0300

    component test style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,54 +5,57 @@ import Image from "next/image";
 
 import { Man, Bg } from "./styles"; 
 import { IoSearch } from "react-icons/io5";
-import Loading from "./loading";
+import Header from "@/components/Header";
 
 
 export default function Home() {
 
   return (
-    <Man>
-      <Bg>
-        <Image src={"/nest.png"} alt="test" fill={true} style={{filter: "brightness(13%)"}}/>
-      </Bg>
+    <>
+      <Header />
+      <Man>
+        <Bg>
+          <Image src={"/nest.png"} alt="test" fill={true} style={{filter: "brightness(13%)"}}/>
+        </Bg>
 
-      <div className="bac">
-        <div className="content">
-          <div className="title" style={{zIndex: "100"}}>
-            <h1>MARVEL <span>API</span></h1>
+        <div className="bac">
+          <div className="content">
+            <div className="title" style={{zIndex: "100"}}>
+              <h1>MARVEL <span>API</span></h1>
+            </div>
+            
+            <section className="options">
+              <Link className="hel" href={'/characters'}>
+                <button>
+                  Characters <br/>
+                  <IoSearch style={{marginTop: 6}}/>
+                </button>
+              </Link>
+
+              <Link className="hel" href={'/comic'}>
+                <button>
+                  Comics <br/>
+                  <IoSearch style={{marginTop: 6}}/>
+                </button>
+              </Link>
+
+              <Link className="hel" href={'/series'}>
+                <button>
+                  Series <br/>
+                  <IoSearch style={{marginTop: 6}}/>
+                </button>
+              </Link>
+
+              <Link className="hel" href={'/even'}>
+                <button>
+                  events <br/>
+                  <IoSearch style={{marginTop: 6}}/>
+                </button>
+              </Link>
+            </section>
           </div>
-          
-          <section className="options">
-            <Link className="hel" href={'/characters'}>
-              <button>
-                Characters <br/>
-                <IoSearch style={{marginTop: 6}}/>
-              </button>
-            </Link>
-
-            <Link className="hel" href={'/comic'}>
-              <button>
-                Comics <br/>
-                <IoSearch style={{marginTop: 6}}/>
-              </button>
-            </Link>
-
-            <Link className="hel" href={'/series'}>
-              <button>
-                Series <br/>
-                <IoSearch style={{marginTop: 6}}/>
-              </button>
-            </Link>
-
-            <Link className="hel" href={'/even'}>
-              <button>
-                events <br/>
-                <IoSearch style={{marginTop: 6}}/>
-              </button>
-            </Link>
-          </section>
         </div>
-      </div>
-    </Man>
+      </Man>
+    </>
   );
 }
