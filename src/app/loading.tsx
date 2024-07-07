9b9953190703bcfec17efef58f45772c03d45829
GustavoCR021 <gustavoc1922@gmail.com>
2024-07-07 18:04:33 -0300

    style: add style for loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,16 @@
+"use client"
+
+import { CircleLoader } from "react-spinners"
+
 export default function Loading() {
   return( 
-    <h1>
-      Loading...
-    </h1>
+    <section style={{width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}>
+      <div style={{width: "100%", height: "100%", background: "#000", display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column"}}>
+        <h1 style={{color: "#fff"}}>
+          Loading...
+        </h1>
+        <CircleLoader color="#ed1a23" size={120} speedMultiplier={1}/>
+      </div>
+    </section>
   )
 }
